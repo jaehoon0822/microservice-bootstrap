596b0e8b849849045eb946ee663554964e6f66a2
jhoon <jaehoon0822@gmail.com>
2023-11-03 12:58:59 +0900

    :construction: Docs: 문서 작성중
    문서 작성중

--- ch2/my-new-project/src/index.ts
+++ ch2/my-new-project/src/index.ts
@@ -1,3 +1,5 @@
+import path from "path";
+import fs from "fs";
 import express, { Request, Response } from "express";
 
 const app = express();
@@ -5,6 +7,26 @@ const port = 8080;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
+});
+
+app.get("/video", (req: Request, res: Response) => {
+  const filePath = path.resolve(
+    __dirname,
+    "./videos/SampleVideo_1280x720_1mb.mp4"
+  );
+
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      console.error("An error occurred");
+      res.sendStatus(500);
+      return;
+    }
+    res.writeHead(200, {
+      "Content-Length": stats.size,
+      "Content-Type": "video/mp4",
+    });
+    fs.createReadStream(filePath).pipe(res);
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port${port}!`));
